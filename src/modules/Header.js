9cbb3f5a25a061db8e158d21ed5bad38d76d51ed
Ilya Kuzimn <ilyacool@mail.ru>
2023-03-21 22:39:20 +0700

    add focus on tag "input", activate search on keydown "enter"

--- src/modules/Header.js
+++ src/modules/Header.js
@@ -6,6 +6,18 @@ export default class Header extends React.Component{
         this.handlerChangeSelectOrder = this.handlerChangeSelectOrder.bind(this);
         this.handlerChangeSelectCategory = this.handlerChangeSelectCategory.bind(this);
         this.handlerChangeTextRequest = this.handlerChangeTextRequest.bind(this);
+        this.handleInputKeyPress = this.handleInputKeyPress.bind(this);
+    }
+
+    componentDidMount(){
+        document.querySelector('input').focus();
+    }
+
+    handleInputKeyPress(e){
+        var key=e.keyCode || e.which;
+        if (key === 13){ // Клавиша Enter
+            this.props.getBooks(document.getElementById("search-string").value);
+        }
     }
 
     handlerChangeSelectOrder(event) {
@@ -19,40 +31,42 @@ export default class Header extends React.Component{
         this.props.setTextRequest(event.target.value);
     }
 
-  render(){
-    return (
-        <header>
+    
 
-            <div className="header__content_center">
-                <h1 className="header__title">Поиск книги</h1>
-                    <div className='className="header__form-search'>
-                        <input type="text" id="search-string" size="100" value={this.props.textRequest} onChange={this.handlerChangeTextRequest}/>
-                        <button onClick={()=>(this.props.getBooks(document.getElementById("search-string").value))}>Поиск</button>
-                    </div>
+    render(){
+        return (
+            <header>
 
-                    <div className='header__form-filter'>
-                        <label className="header__form-label" htmlFor="header__form-category">Категория:</label>                
-                        <select id="header__form-category" onChange={this.handlerChangeSelectCategory}>
-                            {/* список категорий */} 
-                            <option value='all'>all</option>
-                            <option value='art'>art</option>
-                            <option value='biography'>biography</option>
-                            <option value='computers'>computers</option>
-                            <option value='history'>history</option>
-                            <option value='medical'>medical</option>
-                            <option value='poetry'>poetry</option>
-                        </select>
+                <div className="header__content_center">
+                    <h1 className="header__title">Поиск книги</h1>
+                        <div className='className="header__form-search'>
+                            <input type="text" id="search-string" size="100" value={this.props.textRequest} onChange={this.handlerChangeTextRequest} onKeyPress={this.handleInputKeyPress}/>
+                            <button onClick={()=>(this.props.getBooks(document.getElementById("search-string").value))}>Поиск</button>
+                        </div>
 
-                        <label className="header__form-label" htmlFor="header__form-sort">Сортировка:</label>
-                        <select id="header__form-sort" onChange={this.handlerChangeSelectOrder}>
-                            {/* Список сортировки */}
-                            <option value='relevance'>relevance</option>
-                            <option value='newest'>newest</option>
-                        </select>
-                    </div>
-                {/* </form> */}
-            </div>
-        </header>
-    );
-  }
+                        <div className='header__form-filter'>
+                            <label className="header__form-label" htmlFor="header__form-category">Категория:</label>                
+                            <select id="header__form-category" onChange={this.handlerChangeSelectCategory}>
+                                {/* список категорий */} 
+                                <option value='all'>all</option>
+                                <option value='art'>art</option>
+                                <option value='biography'>biography</option>
+                                <option value='computers'>computers</option>
+                                <option value='history'>history</option>
+                                <option value='medical'>medical</option>
+                                <option value='poetry'>poetry</option>
+                            </select>
+
+                            <label className="header__form-label" htmlFor="header__form-sort">Сортировка:</label>
+                            <select id="header__form-sort" onChange={this.handlerChangeSelectOrder}>
+                                {/* Список сортировки */}
+                                <option value='relevance'>relevance</option>
+                                <option value='newest'>newest</option>
+                            </select>
+                        </div>
+                    {/* </form> */}
+                </div>
+            </header>
+        );
+    }
 }
