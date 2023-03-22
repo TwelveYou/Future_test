589117c8b8e71e5d16b3835bb50e2c977fb9dd93
Ilya Kuzimn <ilyacool@mail.ru>
2023-03-22 12:20:31 +0700

    make class component "Header" in functional component

--- src/modules/ButtonAddBooks.js
+++ src/modules/ButtonAddBooks.js
@@ -12,16 +12,3 @@ export default function ButtonAddBooks(props) {
     </div>
   )
 }
-
-
-// import React from "react";
-
-// export default class ButtonAddBooks extends React.Component {
-//   render() {
-//     return (
-//         <div className="button-add">
-//             <button id="button-add__button" onClick={()=>(this.props.addBooks(document.getElementById("search-string").value))}>Загрузить еще 30 книг</button>        
-//         </div>
-//     )
-//   }
-// }
