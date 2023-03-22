d3e1b8de95ecea537e19da69cf30ed95e396aee7
Ilya Kuzimn <ilyacool@mail.ru>
2023-03-22 12:12:11 +0700

    make class component "ButtonAddBooks" in functional component

--- src/modules/ButtonAddBooks.js
+++ src/modules/ButtonAddBooks.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React from 'react'
 
-export default class ButtonAddBooks extends React.Component {
-  render() {
-    return (
-        <div className="button-add">
-            <button id="button-add__button" onClick={()=>(this.props.addBooks(document.getElementById("search-string").value))}>Загрузить еще 30 книг</button>        
-        </div>
-    )
-  }
+export default function ButtonAddBooks(props) {
+  return (
+    <div className="button-add">
+      <button 
+        id="button-add__button" 
+        onClick={()=>(props.addBooks(document.getElementById("search-string").value))}
+      >
+        Загрузить еще 30 книг
+      </button>        
+    </div>
+  )
 }
+
+
+// import React from "react";
+
+// export default class ButtonAddBooks extends React.Component {
+//   render() {
+//     return (
+//         <div className="button-add">
+//             <button id="button-add__button" onClick={()=>(this.props.addBooks(document.getElementById("search-string").value))}>Загрузить еще 30 книг</button>        
+//         </div>
+//     )
+//   }
+// }
