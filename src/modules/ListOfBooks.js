e2da937344eef102f9db77d954de522cca8a2ac6
Ilya Kuzimn <ilyacool@mail.ru>
2023-03-22 12:30:23 +0700

    make class component "ListOfBooks" in functional component

--- src/modules/ListOfBooks.js
+++ src/modules/ListOfBooks.js
@@ -1,47 +1,68 @@
-import React from 'react';
+import React from 'react'
 import Book from './Book';
 
-export default class ListOfBooks extends React.Component{
-  constructor(props) {
-    super(props);
-    this.getSafeUrlImg = this.getSafeUrlImg.bind(this);
-  }
+export default function ListOfBooks(props) {
 
-  getSafeUrlImg(book){
+  function safeGetUrlImg(book){
     if(book.volumeInfo.hasOwnProperty('imageLinks')){
       return book.volumeInfo.imageLinks.thumbnail;
     } else 
       return '';
   }
-      
-  render(){
+
+  function showListOgBooks(){
     let list;
 
-    if(this.props.books === null){
+    if(props.books === null){
       list = <h3> Введите название книги </h3>;
     }
-    // else  
-    // if(this.props.books === undefined){
-    //   list = <h3> Что-то пошло не так и мы не смогли загрузить эти книги. </h3>;
-    // }
     else{
-      list = <div className="list"> {this.props.books.map((book, index)=>(
+      list = <div className="list"> {props.books.map((book, index)=>(
         <Book 
           key={'ID'+book.id+'-'+index}
           title={book.volumeInfo.title  } 
           category={book.volumeInfo.categories} 
           authors={book.volumeInfo.authors} 
-          url={this.getSafeUrlImg(book)}
-          setOpenBook={this.props.setOpenBook}
+          url={safeGetUrlImg(book)}
+          setOpenBook={props.setOpenBook}
           book={book}
         />))} 
       </div>;
     }
-
-    return (
-        <div className="list">
-          {list}
-        </div>
-    );
+    return list;
   }
+
+  return (
+    <div className="list">
+      {showListOgBooks()}
+    </div>
+  )
 }
+
+
+// import React from 'react';
+// import Book from './Book';
+
+// export default class ListOfBooks extends React.Component{
+//   constructor(props) {
+//     super(props);
+//     this.getSafeUrlImg = this.getSafeUrlImg.bind(this);
+//   }
+
+//   getSafeUrlImg(book){
+//     if(book.volumeInfo.hasOwnProperty('imageLinks')){
+//       return book.volumeInfo.imageLinks.thumbnail;
+//     } else 
+//       return '';
+//   }
+      
+//   render(){
+
+
+//     return (
+//         <div className="list">
+//           {list}
+//         </div>
+//     );
+//   }
+// }
