1378b92938962468990815d13223fab07c66370b
Ilya Kuzimn <ilyacool@mail.ru>
2023-03-22 12:42:46 +0700

    Before use Redux

--- src/index.js
+++ src/index.js
@@ -4,10 +4,32 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+
+const defaultState = { // начальное состояние хранилища
+  cash: 0,
+};
+
+const reducer = (state = defaultState, action) => {
+  switch(action.type){
+      case "ADD_CASH": 
+          return {...state, cash: state.cash + action.payLoader};
+      case "GET_CASH": 
+          return {...state, cash: state.cash - action.payLoader};
+      default: 
+          return state;
+  }
+}
+
+const store = createStore(reducer);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
